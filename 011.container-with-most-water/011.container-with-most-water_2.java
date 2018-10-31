fa10743ad88f541bb0eff095fd6bcf3882860af6
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2018-10-31 18:03:19 +0800

    Create 011.container-with-most-water_2.java

---
+++ 011.container-with-most-water/011.container-with-most-water_2.java
@@ -0,0 +1,19 @@
+//方法2（Java版）：双指针法  
+//这种方法背后的思路在于，两线段之间形成的区域总是会受到其中较短那条长度的限制。此外，两线段距离越远，得到的面积就越大。
+//我们在由线段长度构成的数组中使用两个指针，一个放在开始，一个置于末尾。 此外，我们会使用变量 maxarea
+//maxarea 来持续存储到目前为止所获得的最大面积。 在每一步中，我们会找出指针所指向的两条线段形成的区域，更新 maxarea
+//maxarea，并将指向较短线段的指针向较长线段那端移动一步。
+
+public class Solution {
+    public int maxArea(int[] height) {
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+    }
+}
