f39a2c7174b74433f60d8959f0f420c5662cf584
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2018-11-01 19:39:18 +0800

    Create 014.integer-to-roman_3.py

---
+++ 014.integer-to-roman/014.integer-to-roman_3.py
@@ -0,0 +1,20 @@
+#欢迎关注微信公众号：AI数据分析算法
+#来自群成员 “辰星” 的方法
+class Solution: 
+    def intToRoman(self,num): 
+    '''
+    :type num:int
+    :rtype:str n4
+    '''
+    rtn=''
+    conv_tab=(1000,100,10,1)
+    conv_value={1000:('','M','MM','MMM'),100:
+    (",'C','CC','CCC','CD','D','DC',"DCC','DCCC','CM'),10:
+    (",'X','XX','XXX','XL','L','LX','LXX','LXXX','XC'),1:
+    (",'I','II','III','IV','V','VI','VII','VIII','IX')}
+    for base in conv_tab: 
+      cnt=num //base
+      num=num % base 
+      rtn+=conv_value[base][cnt]
+      return rtn
+      
