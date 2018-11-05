9de980badd953868d2081f924ab8e5811c604d77
sanxiu <33210472+sanxiux@users.noreply.github.com>
2018-11-05 16:36:39 +0800

    Update 014.Longest Common Prefix.py

--- 014.Longest Common Prefix/014.Longest Common Prefix.py
+++ 014.Longest Common Prefix/014.Longest Common Prefix.py
@@ -1,1 +1,11 @@
 
+class Solution:
+    # @return a string
+    def longestCommonPrefix(self, strs):
+        if strs == []:
+            return ''
+        for i in range(len(strs[0])):
+            for str in strs:
+                if len(str) <= i or str[i] != strs[0][i]:
+                    return strs[0][:i]
+        return strs[0]
