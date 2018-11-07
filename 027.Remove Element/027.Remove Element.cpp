676bf309d08dea7cd09068796c44425cf1814723
sanxiu <33210472+sanxiux@users.noreply.github.com>
2018-11-07 21:46:02 +0800

    Update 027.Remove Element.cpp

--- 027.Remove Element/027.Remove Element.cpp
+++ 027.Remove Element/027.Remove Element.cpp
@@ -1,1 +1,12 @@
-
+//----------cpp------------
+class Solution {
+	public:
+		int removeElement(vector<int>& nums, int val) {
+			int n = nums.size(), p = 0;
+			for (int i = 0; i < n; i++) {
+				if (val != nums[i])
+					nums[p++] = nums[i];
+			}
+			return p;
+		}
+};
