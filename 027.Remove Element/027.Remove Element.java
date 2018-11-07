30c457ab5e1ec11f87a7a62355b24514acdead8d
sanxiu <33210472+sanxiux@users.noreply.github.com>
2018-11-07 21:48:50 +0800

    Update 027.Remove Element.java

--- 027.Remove Element/027.Remove Element.java
+++ 027.Remove Element/027.Remove Element.java
@@ -1,1 +1,14 @@
 
+//----------------java--------------------
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        //{1,1,2,1,2,3,2,3,1}
+        int newIndex = 0;
+        for (int oldIndex = 0; oldIndex < A.length; ++oldIndex) {
+            if (A[oldIndex] != elem) {
+                A[newIndex++] = A[oldIndex];
+            } 
+        }
+        return newIndex;
+    }
+}
