70c632fcd887915456a87d782218dfc2b3471d8d
sanxiu <33210472+sanxiux@users.noreply.github.com>
2018-11-08 20:56:17 +0800

    Update 028.Implement strStr().py

--- 028.Implement strStr()/028.Implement strStr().py
+++ 028.Implement strStr()/028.Implement strStr().py
@@ -1,1 +1,14 @@
 
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        len1=len(haystack)
+        len2=len(needle)
+        for i in range(len1-len2+1):
+            if haystack[i:i+len2]==needle:
+                return i
+        return -1
