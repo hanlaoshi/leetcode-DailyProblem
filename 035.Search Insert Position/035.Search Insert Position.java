691ffc69523d62e0218f3682ef64b19da6a72f2a
sanxiu <33210472+sanxiux@users.noreply.github.com>
2018-11-09 17:40:14 +0800

    Update 035.Search Insert Position.java

--- 035.Search Insert Position/035.Search Insert Position.java
+++ 035.Search Insert Position/035.Search Insert Position.java
@@ -1,1 +1,25 @@
-
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+    	//特殊情况
+        if(nums.length == 0 || nums[0] > target)
+            return 0;
+        if(nums[nums.length - 1] < target)
+        	return nums.length;
+        //先二分查找，找到了就返回
+        int low = 0;
+        int hight = nums.length - 1;
+        int mid = 0;
+        while(low <= hight){
+            mid = (low + hight)/2;
+            if(nums[mid] < target){
+                low = mid + 1;
+            }else if(nums[mid] > target){
+                hight = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+        //找不到再判断与nums[mid]大小，返回索引
+        return nums[mid] > target?mid:mid+1;
+    }
+}
