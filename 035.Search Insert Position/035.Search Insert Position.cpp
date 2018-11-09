33ff0ff865b2e65f1c3ba921eb7df7a57494d01b
sanxiu <33210472+sanxiux@users.noreply.github.com>
2018-11-09 17:48:25 +0800

    Update 035.Search Insert Position.cpp

--- 035.Search Insert Position/035.Search Insert Position.cpp
+++ 035.Search Insert Position/035.Search Insert Position.cpp
@@ -1,1 +1,17 @@
 
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1,pos=0,mid;
+        while(l<=r){
+            mid=(l+r)>>1;
+            if(nums[mid]==target)return mid;
+            else if(target<nums[mid]){
+                r=mid-1;pos=mid;
+            }else{
+                l=mid+1;pos=mid+1;
+            }
+        }
+        return pos;
+    }
+};
