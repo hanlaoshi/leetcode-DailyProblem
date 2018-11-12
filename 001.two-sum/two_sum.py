c2b2805c418baa55c50bc31b2fe64e28631d8f56
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2018-11-12 22:56:59 +0800

    Update two_sum.py

--- 001.two-sum/two_sum.py
+++ 001.two-sum/two_sum.py
@@ -2,7 +2,7 @@ class Solution:
     # @return a tuple, (index1, index2)
     def twoSum(self, num, target):
         dict = {}
-        for i in xrange(len(num)):
+        for i in range(len(num)):
             x = num[i]
             if target-x in dict:
                 return (dict[target-x]+1, i+1)
