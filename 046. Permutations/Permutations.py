a0492f399fb5559d0eedb3fdd35b28f1c4a20e9e
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2018-11-15 09:20:48 +0800

    Update Permutations.py

--- 046. Permutations/Permutations.py
+++ 046. Permutations/Permutations.py
@@ -1,4 +1,27 @@
 '''
-
+解题思路
+这道题比较单间，采用递归把数组中的数字依次加入当前数组current来进行排列组合。
 
 '''
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.get_permute([], nums, result)
+        return result
+
+    def get_permute(self, current, num, result):
+        if not num:
+            result.append(current + [])
+            return
+        for i, v in enumerate(num):
+            current.append(num[i])
+            self.get_permute(current, num[:i] + num[i + 1:], result)
+            current.pop()
+
+
+if __name__ == "__main__":
+    assert Solution().permute([1, 2, 3]) 
