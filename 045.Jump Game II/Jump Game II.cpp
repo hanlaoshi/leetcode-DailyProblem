16ab4ba8ec23c1b97269d26c21f70aab49840730
sanxiu <33210472+sanxiux@users.noreply.github.com>
2018-11-19 21:29:17 +0800

    Update Jump Game II.cpp

--- 045.Jump Game II/Jump Game II.cpp
+++ 045.Jump Game II/Jump Game II.cpp
@@ -1,32 +1,39 @@
-//leetcode上大牛的解法
-/*
-大牛这个算法的思想主要是，扫描数组，以确定当前最远能覆盖的节点，放入curr。
-然后继续扫描，直到当前的路程超过了上一次算出的覆盖范围，那么更新覆盖范围，同时更新条数，因为我们是经过了多一跳才能继续前进的。
-
-形象地说，这个是在争取每跳最远的greedy。
-*/
-
-/*
- * We use "last" to keep track of the maximum distance that has been reached
- * by using the minimum steps "ret", whereas "curr" is the maximum distance
- * that can be reached by using "ret+1" steps. Thus,
- * curr = max(i+A[i]) where 0 <= i <= last.
- */
- 
+--c++-------------这是运行速度最快的 ，大牛写的 ，看不懂 不会c [囧]
+static const auto init = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return nullptr;
+}();
 class Solution {
 public:
-    int jump(int A[], int n) {
-        int ret = 0;
-        int last = 0;
-        int curr = 0;
-        for (int i = 0; i < n; ++i) {
-            if (i > last) {
-                last = curr;
-                ++ret;
-            }
-            curr = max(curr, i+A[i]);
-        }
-
-        return ret;
+    int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1)
+			return 1;
+		vector<vector<int>> p;
+        for (int i = 0; i < m; i++)
+		{
+			vector<int> vec(n);
+			p.push_back(vec);
+		}
+		p[0][0] = 0;
+		p[1][0] = 1;
+		p[0][1] = 1;
+		//p[1][1] = 0;
+		for (int i = 0; i < n; i++)
+		{
+			p[0][i] = 1;
+		}
+		for (int i = 0; i < m; i++)
+		{
+			p[i][0] = 1;
+		}
+		for (int i = 1; i < m; i++)
+		{
+			for (int j = 1; j < n; j++)
+			{
+				p[i][j] = p[i - 1][j] + p[i][j - 1];
+			}
+		}
+		return p[m - 1][n - 1];
     }
 };
