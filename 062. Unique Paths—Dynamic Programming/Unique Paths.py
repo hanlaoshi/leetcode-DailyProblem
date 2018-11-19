f6ddc53b65b8b59a1f269b83e4555f1cdfcd318d
sanxiu <33210472+sanxiux@users.noreply.github.com>
2018-11-19 22:01:10 +0800

    Update Unique Paths.py

--- 062. Unique Paths—Dynamic Programming/Unique Paths.py
+++ 062. Unique Paths—Dynamic Programming/Unique Paths.py
@@ -19,3 +19,18 @@ class Solution:
             return n
 
         return recursion_paths(m - 1, n - 1)
+#-------------方法二python----------------------
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] nums = new int[m][n];
+        for (int i = 0 ;i < m;i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == 0 || j == 0)
+                    nums[i][j] = 1;
+                else
+                    nums[i][j] = nums[i - 1][j] + nums[i][j - 1];
+            }
+        }
+        return nums[m - 1][n - 1];
+    }
+}
