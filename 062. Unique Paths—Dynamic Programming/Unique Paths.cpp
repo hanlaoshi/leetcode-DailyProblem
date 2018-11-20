a2d2dcb0c38a9614cf8ba5f684d6fb604e3c7b3a
sanxiu <33210472+sanxiux@users.noreply.github.com>
2018-11-20 17:54:22 +0800

    Update Unique Paths.cpp

--- 062. Unique Paths—Dynamic Programming/Unique Paths.cpp
+++ 062. Unique Paths—Dynamic Programming/Unique Paths.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
-    int dp[105][105];
     int uniquePaths(int m, int n) {
+        vector<vector<int>> info(n,vector<int>(m,0));
+        for(int i=0;i<m;++i)
+        {
+            info[0][i]=1;
+        }
         for(int i=0;i<n;++i)
-            for(int j=0;j<m;++j)
-                dp[i][j]=i*j==0?1:(dp[i-1][j]+dp[i][j-1]);
-         return dp[n-1][m-1];
+        {
+            info[i][0]=1;
+        }
+        for(int i=1;i<m;++i)
+        {
+            for(int j=1;j<n;++j)
+            {
+                info[j][i]=info[j-1][i]+info[j][i-1];
+            }
+        }
+        return info[n-1][m-1];
     }
 };
