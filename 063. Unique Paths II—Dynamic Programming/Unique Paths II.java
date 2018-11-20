c79ee77287375a1de24f69f6943b564e0070e2c5
sanxiu <33210472+sanxiux@users.noreply.github.com>
2018-11-20 17:11:58 +0800

    Update Unique Paths II.java

--- 063. Unique Paths II—Dynamic Programming/Unique Paths II.java
+++ 063. Unique Paths II—Dynamic Programming/Unique Paths II.java
@@ -1,38 +1,15 @@
-/*
-这道题就用循环递归解决子问题。
-
-因为求所有组合，这就意味着不能重复使用元素，要用visited数组。
-
-有因为是所有可能的组合，所以循环length次，就是这里面每位都有可能有length个可能性。
-
-正因为如此，每一层递归就不需要传递一个start点，告诉他从哪开始（因为都是从头开始循环）。
-*/
-
-public ArrayList<ArrayList<Integer>> permute(int[] num) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> item = new ArrayList<Integer>();
-        
-        if(num.length==0||num==null)
-            return res;
-        boolean[] visited = new boolean[num.length];  
-        
-        permutation_helper(num,res,item,visited);
-        return res;
-    }
-    
-    public void permutation_helper(int[] num, ArrayList<ArrayList<Integer>> res, ArrayList<Integer> item,boolean[] visited){
-        if(item.size()==num.length){
-            res.add(new ArrayList<Integer>(item));
-            return;
-        }
-        
-        for(int i = 0; i<num.length;i++){
-            if(!visited[i]){
-                item.add(num[i]);
-                visited[i]=true;
-                permutation_helper(num,res,item,visited);
-                item.remove(item.size()-1);
-                visited[i]=false;
-            }
-        }
-    }
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        if not len(obstacleGrid) or not len(obstacleGrid[0]): return 0
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0] * (n+1) for _ in xrange(m+1)]
+        dp[0][1] = 1
+        for r in xrange(1, m+1):
+            for c in xrange(1, n+1):
+                if obstacleGrid[r-1][c-1] == 0:
+                    dp[r][c] = dp[r-1][c] + dp[r][c-1]
+        return dp[-1][-1]
