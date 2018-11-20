238092e66579b517fc478081f9a606cd3ef2d25c
sanxiu <33210472+sanxiux@users.noreply.github.com>
2018-11-20 17:27:12 +0800

    Update Unique Paths II.java

--- 063. Unique Paths II—Dynamic Programming/Unique Paths II.java
+++ 063. Unique Paths II—Dynamic Programming/Unique Paths II.java
@@ -1,15 +1,26 @@
-class Solution(object):
-    def uniquePathsWithObstacles(self, obstacleGrid):
-        """
-        :type obstacleGrid: List[List[int]]
-        :rtype: int
-        """
-        if not len(obstacleGrid) or not len(obstacleGrid[0]): return 0
-        m, n = len(obstacleGrid), len(obstacleGrid[0])
-        dp = [[0] * (n+1) for _ in xrange(m+1)]
-        dp[0][1] = 1
-        for r in xrange(1, m+1):
-            for c in xrange(1, n+1):
-                if obstacleGrid[r-1][c-1] == 0:
-                    dp[r][c] = dp[r-1][c] + dp[r][c-1]
-        return dp[-1][-1]
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1] == 1)
+            return 0;
+        int[][] ways = new int[obstacleGrid.length][obstacleGrid[0].length];
+		int i = 0;
+		int j = 0;
+		for (i = 0; i < obstacleGrid.length; i++) {
+			for (j = 0; j < obstacleGrid[0].length; j++) {
+				if (obstacleGrid[i][j] == 1) {
+					ways[i][j] = 0;
+				} else if (i == 0 && j == 0) {
+					ways[i][j] = 1;
+				} else if (i == 0 && j > 0) {
+					ways[i][j] = ways[i][j - 1];
+				} else if (j == 0 && i > 0) {
+					ways[i][j] = ways[i - 1][j];
+				} else {
+					ways[i][j] = ways[i - 1][j] + ways[i][j - 1];
+				}
+ 
+			}
+		}
+		return ways[i - 1][j - 1];
+    }
+}
