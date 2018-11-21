0799459e6d61f8370c6c6a72c56afa9b07b79d15
sanxiu <33210472+sanxiux@users.noreply.github.com>
2018-11-21 19:06:57 +0800

    Update Minimum Path Sum.java

--- 064. Minimum Path Sum—Dynamic Programming/Minimum Path Sum.java
+++ 064. Minimum Path Sum—Dynamic Programming/Minimum Path Sum.java
@@ -1,38 +1,53 @@
-/*
-这道题就用循环递归解决子问题。
-
-因为求所有组合，这就意味着不能重复使用元素，要用visited数组。
-
-有因为是所有可能的组合，所以循环length次，就是这里面每位都有可能有length个可能性。
-
-正因为如此，每一层递归就不需要传递一个start点，告诉他从哪开始（因为都是从头开始循环）。
-*/
-
-public ArrayList<ArrayList<Integer>> permute(int[] num) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> item = new ArrayList<Integer>();
-        
-        if(num.length==0||num==null)
-            return res;
-        boolean[] visited = new boolean[num.length];  
-        
-        permutation_helper(num,res,item,visited);
-        return res;
-    }
-    
-    public void permutation_helper(int[] num, ArrayList<ArrayList<Integer>> res, ArrayList<Integer> item,boolean[] visited){
-        if(item.size()==num.length){
-            res.add(new ArrayList<Integer>(item));
-            return;
+#-------------第一种---------------
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0){
+            return 0;
         }
-        
-        for(int i = 0; i<num.length;i++){
-            if(!visited[i]){
-                item.add(num[i]);
-                visited[i]=true;
-                permutation_helper(num,res,item,visited);
-                item.remove(item.size()-1);
-                visited[i]=false;
+        int row = grid.length;
+        int col = grid[0].length;
+        int[][] dp = new int[row][col];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < row; i++) {
+			dp[i][0] = dp[i - 1][0] + grid[i][0];
+		}
+		for (int j = 1; j < col; j++) {
+			dp[0][j] = dp[0][j - 1] + grid[0][j];
+		}
+		for (int i = 1; i < row; i++) {
+			for (int j = 1; j < col; j++) {
+				dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+			}
+		}
+		return dp[row - 1][col - 1];
+    }
+}
+#-------------第二种leetcode最快的---------------
+class Solution {
+        private int[][] grid;
+        private int sum;
+        private int[][] temp;
+        public int minPathSum(int[][] grid) {
+            this.grid=grid;
+            temp=new int[grid.length][grid[0].length] ;
+            int x=0;
+            int y=0;
+
+
+
+            return minPath(0,0);
+        }
+        private int minPath(int x,int  y){
+            if(x==grid.length-1&&y==grid[0].length-1){
+                return grid[x][y];
             }
+            if(x>grid.length-1||y>grid[0].length-1){
+                return Integer.MAX_VALUE;
+            }
+            if(temp[x][y]!=0){
+                return temp[x][y];
+            }
+            temp[x][y]=grid[x][y]+Math.min(minPath(x+1,y),minPath(x,y+1));
+            return temp[x][y];
         }
-    }
+}
