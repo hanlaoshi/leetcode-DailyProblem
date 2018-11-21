74c9a20f76bccb0245d7ca7362e97818ba5c9178
sanxiu <33210472+sanxiux@users.noreply.github.com>
2018-11-21 18:57:42 +0800

    Update Minimum Path Sum.py

--- 064. Minimum Path Sum—Dynamic Programming/Minimum Path Sum.py
+++ 064. Minimum Path Sum—Dynamic Programming/Minimum Path Sum.py
@@ -19,3 +19,20 @@ class Solution:
             for j in range(1, m):
                 min_grid[i][j] = min(min_grid[i - 1][j],min_grid[i][j - 1]) + grid[i][j]
         return min_grid[n - 1][m - 1]
+#-----------------python快速法--------------------
+class Solution:
+    def minPathSum(self, grid):
+        n=len(grid[0])
+        m=len(grid)
+        dp = [[0]*n for i in range(m)]
+        dp[0][0]=grid[0][0]
+        for i in range(1,m):
+            dp[i][0]=grid[i][0]+dp[i-1][0]
+        for j in range(1,n):
+            dp[0][j]=grid[0][j]+dp[0][j-1]
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j]=min(dp[i][j-1],dp[i-1][j])+grid[i][j];
+        return dp[m-1][n-1]              
+        
+        
