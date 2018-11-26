02fada65ffa3bae13a99679e6d934a3ff46d1eb1
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2018-11-26 20:34:54 +0800

    Update 100. Same Tree.py

--- 100. Same Tree/100. Same Tree.py
+++ 100. Same Tree/100. Same Tree.py
@@ -60,3 +60,27 @@ class Solution:
                 return False
             
         return ChkSameTree(p, q)
+    
+    
+    
+    # 第三种方法  by 多年未见大佬出品
+    
+    # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not p and not q: return True
+        if not p: return False
+        if not q: return False
+        if p.val != q.val: return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
