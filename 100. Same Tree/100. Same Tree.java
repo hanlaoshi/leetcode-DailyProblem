66e7bd9d2e3899bff677705036cde0bd2bb229aa
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2018-11-26 20:30:40 +0800

    Update 100. Same Tree.java

--- 100. Same Tree/100. Same Tree.java
+++ 100. Same Tree/100. Same Tree.java
@@ -38,3 +38,48 @@ public boolean isSameTree(TreeNode p, TreeNode q) {
 
 
 */
+
+//第三种方法，by 三休大佬做法
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    boolean flag = true;
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null&& q==null)
+            return true;
+        if((p != null && q == null) || (p == null && q != null))
+            return false;
+
+        isSame(p,q);
+        return flag;
+    }
+    
+    
+    public void isSame(TreeNode p,TreeNode q){
+         if(p.val != q.val)
+            flag = false;
+        
+        if ((p.left == null && q.left != null) || (p.left != null && q.left == null))
+            flag = false;
+        
+        if (p.left != null && q.left != null)
+            isSame(p.left,q.left);
+        
+        if((p.right == null && q.right != null) || (p.right != null && q.right==null))
+            flag = false;
+
+        if (p.right != null && q.right != null)
+            isSame(p.right,q.right);
+        
+        
+    }
+}
