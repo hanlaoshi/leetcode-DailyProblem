7ddc6e956005085e929434109c7288663b20e7c8
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2018-11-30 21:39:41 +0800

    Update 112. Path Sum.py

--- 112. Path Sum/112. Path Sum.py
+++ 112. Path Sum/112. Path Sum.py
@@ -27,3 +27,45 @@ class Solution(object):
 
 if __name__ == "__main__":
     None
+
+    # 方法2   辰星大佬 出品
+    
+    # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root: return False
+        path_sum, dp, cnt = [[root.val]], [[root]], 0
+        while True:
+            nxt = cnt + 1
+            dp.append([])
+            path_sum.append([])
+            for i, node in enumerate(dp[cnt]):
+                if path_sum[cnt][i] == sum and not node.left and not node.right:
+                    return True
+                if node.left:
+                    path_sum[nxt].append(path_sum[cnt][i] + node.left.val)
+                    dp[nxt].append(node.left)
+                if node.right:
+                    path_sum[nxt].append(path_sum[cnt][i] + node.right.val)
+                    dp[nxt].append(node.right)
+            if dp[nxt] == []:
+                break
+            cnt = nxt
+        return False
+    
+  
+
+
+#  方法3   多年文件大佬 出品
+    
