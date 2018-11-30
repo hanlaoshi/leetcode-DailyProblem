5dc1626741dd4ce5180684fc08fbc0fef2667732
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2018-11-30 21:39:51 +0800

    Update 112. Path Sum.py

--- 112. Path Sum/112. Path Sum.py
+++ 112. Path Sum/112. Path Sum.py
@@ -68,4 +68,24 @@ class Solution:
 
 
 #  方法3   多年文件大佬 出品
-    
+    # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root: return False;
+        if root.left: 
+            if self.hasPathSum(root.left, sum - root.val): return True
+        if root.right: 
+            if self.hasPathSum(root.right, sum - root.val): return True
+        if (not root.left) and (not root.right) and sum == root.val: return True
+        return False
