b1a8f2410d38fd205d2a5fddb539d122f42a7cd3
sanxiu <33210472+sanxiux@users.noreply.github.com>
2018-12-06 22:03:50 +0800

    Update 075.Sort Colors.js

--- 075.Sort Colors/075.Sort Colors.js
+++ 075.Sort Colors/075.Sort Colors.js
@@ -1,4 +1,4 @@
-//---------------狐狸-------------------------
+//---------------岁月-------------------------
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
