cda180d3a67888327f09e94ecbd26c5064b43892
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2018-12-09 22:50:08 +0800

    Create 202. Happy Number.java

---
+++ 202. Happy Number/202. Happy Number.java
@@ -0,0 +1,23 @@
+/*
+按照题目的描述，需要循环求平方和，即求出当前数组的平方和后，再以此平方和作为新的数继续求平方和，而循环终止条件是：
+得到的平方和为1，或得到的平方和在之前的循环中出现过。判断平方和是否为1很简单，每次检查就好了；而判断平方和是否出现过，
+则只需要维持一个Set，每次循环检查当前平方和是否在Set中，在则终止循环，不在则将此平方和放到Set中。
+
+*/
+
+public class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> got = new HashSet<>();
+        while (n != 1 && !got.contains(n)) {
+            got.add(n);
+            int sum = 0;
+            while (n != 0) {
+                sum += Math.pow(n % 10, 2);
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n == 1;
+    }
+}
+
