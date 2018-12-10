54d823cecb63f2cb66e75ff8435d98eac802f452
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2018-12-10 21:00:05 +0800

    Update 202. Happy Number.java

--- 202. Happy Number/202. Happy Number.java
+++ 202. Happy Number/202. Happy Number.java
@@ -21,3 +21,25 @@ public class Solution {
     }
 }
 
+
+//方法二   by 初夏大佬
+
+class Solution {
+    public int reverse(int x) {
+        if((x<=0 && x>-10)||(x<10&&x>0)){
+            return x;
+        }
+        
+        long result = 0;
+        while(x != 0 ){
+            result = result*10 + x%10;
+            x = x/10;
+        }
+        
+        if(result <= Integer.MIN_VALUE || result >= Integer.MAX_VALUE){
+            return 0;
+        }
+        
+        return (int)result;
+    }
+}
