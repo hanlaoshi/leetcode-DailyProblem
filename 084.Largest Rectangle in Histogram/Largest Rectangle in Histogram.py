a947fb7101d33a289e7ad383f9e034597d8ba1ae
XWM <919343782@qq.com>
2019-01-09 21:50:06 +0800

    Signed-off-by: XWM <919343782@qq.com>

---
+++ 084.Largest Rectangle in Histogram/Largest Rectangle in Histogram.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        stack = list()
+        res = 0
+        heights.append(0)
+        N = len(heights)
+        for i in range(N):
+            if not stack or heights[i] > heights[stack[-1]]:
+                stack.append(i)
+            else:
+                while stack and heights[i] <= heights[stack[-1]]:
+                    h = heights[stack[-1]]
+                    stack.pop()
+                    w = i if not stack else i - stack[-1] - 1
+                    res = max(res, h * w)
+                stack.append(i)
+        return res
