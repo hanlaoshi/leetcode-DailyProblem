4a3307cbe3f7638ee5183740fba4f314499bde2b
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2019-01-14 09:37:10 +0800

    Create 232. Implement Queue using Stacks.java

---
+++ 232. Implement Queue using Stacks/232. Implement Queue using Stacks.java
@@ -0,0 +1,37 @@
+class MyQueue {
+  
+    Stack<Integer> temp = new Stack<Integer>();
+    Stack<Integer> value = new Stack<Integer>();
+  
+    // Push element x to the back of queue.
+    public void push(int x) {
+        if(value.isEmpty()){
+            value.push(x);
+        }else{
+            while(!value.isEmpty()){
+                temp.push(value.pop());
+            }
+  
+            value.push(x);
+  
+            while(!temp.isEmpty()){
+                value.push(temp.pop());
+            }   
+        }
+    }
+  
+    // Removes the element from in front of queue.
+    public void pop() {
+        value.pop();
+    }
+  
+    // Get the front element.
+    public int peek() {
+        return value.peek();
+    }
+  
+    // Return whether the queue is empty.
+    public boolean empty() {
+        return value.isEmpty();
+    }
+}　　
