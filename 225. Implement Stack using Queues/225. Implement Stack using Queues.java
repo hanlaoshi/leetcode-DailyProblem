effb725d90e3f0ab68c5ef8ad1938f4792f4c821
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2019-01-14 09:46:56 +0800

    Create 225. Implement Stack using Queues.java

---
+++ 225. Implement Stack using Queues/225. Implement Stack using Queues.java
@@ -0,0 +1,53 @@
+// 使用双队列
+
+class MyStack {
+    LinkedList<Integer> queue1 = new LinkedList<Integer>();
+    LinkedList<Integer> queue2 = new LinkedList<Integer>();
+ 
+    // Push element x onto stack.
+    public void push(int x) {
+        if(empty()){
+            queue1.offer(x);
+        }else{
+            if(queue1.size()>0){
+                queue2.offer(x);
+                int size = queue1.size();
+                while(size>0){
+                    queue2.offer(queue1.poll());
+                    size--;
+                }
+            }else if(queue2.size()>0){
+                queue1.offer(x);
+                int size = queue2.size();
+                while(size>0){
+                    queue1.offer(queue2.poll());
+                    size--;
+                }
+            }
+        }
+    }
+ 
+    // Removes the element on top of the stack.
+    public void pop() {
+        if(queue1.size()>0){
+            queue1.poll();
+        }else if(queue2.size()>0){
+            queue2.poll();
+        }
+    }
+ 
+    // Get the top element.
+    public int top() {
+       if(queue1.size()>0){
+            return queue1.peek();
+        }else if(queue2.size()>0){
+            return queue2.peek();
+        }
+        return 0;
+    }
+ 
+    // Return whether the stack is empty.
+    public boolean empty() {
+        return queue1.isEmpty() & queue2.isEmpty();
+    }
+}
