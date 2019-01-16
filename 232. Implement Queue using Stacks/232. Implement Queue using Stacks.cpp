430ee4d3b3082eac6b33f40aef421ab3a3f83166
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2019-01-16 20:47:11 +0800

    Update 232. Implement Queue using Stacks.cpp

--- 232. Implement Queue using Stacks/232. Implement Queue using Stacks.cpp
+++ 232. Implement Queue using Stacks/232. Implement Queue using Stacks.cpp
@@ -32,3 +32,30 @@ public:
 private:
     stack<int> s;
 };
+
+
+
+//方法二  by多年未见
+class Solution
+{
+public:
+    void push(int node) {
+        stack2.push(node);
+    }
+
+    int pop() {
+        if (stack1.empty()) {
+            while (!stack2.empty()) {
+                stack1.push(stack2.top());
+                stack2.pop();
+            }
+        }
+        int tmp = stack1.top();
+        stack1.pop();
+        return tmp;
+    }
+
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+};
