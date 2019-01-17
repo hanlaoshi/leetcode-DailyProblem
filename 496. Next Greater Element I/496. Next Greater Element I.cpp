a946f30bcaa05bc4f3c683b2b5dc8d3e583a7dea
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2019-01-17 19:25:48 +0800

    Update 496. Next Greater Element I.cpp

--- 496. Next Greater Element I/496. Next Greater Element I.cpp
+++ 496. Next Greater Element I/496. Next Greater Element I.cpp
@@ -70,3 +70,28 @@ public int[] nextGreaterElement(int[] findNums, int[] nums) {
 		return res;
     }
 
+
+//方法三  多年未见大佬
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
+        map<int, int> mp;
+        int cc = 1;
+        int n = findNums.size(), m = nums.size();
+        for (int i = 0; i < n; i++) {
+            mp[findNums[i]] = cc++;
+        }
+        vector<int> ans(n, -1);
+        stack<int> s;
+        for (int i = 0; i < m; i++) {
+            while (!s.empty() && nums[s.top()] < nums[i]) {
+                int tmp = mp[nums[s.top()]];
+                if (tmp) ans[tmp - 1] = nums[i];
+                s.pop();
+            }
+            s.push(i);
+        }
+        while (!s.empty()) s.pop();
+        return ans;
+    }
+};
