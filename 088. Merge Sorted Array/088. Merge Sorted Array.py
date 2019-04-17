5685c1d395ef3a489266ecbec8850e263354aa0c
AifuHan <38586167+hanlaoshi@users.noreply.github.com>
2019-04-17 22:07:53 +0800

    Update 088. Merge Sorted Array.py

--- 088. Merge Sorted Array/088. Merge Sorted Array.py
+++ 088. Merge Sorted Array/088. Merge Sorted Array.py
@@ -20,3 +20,24 @@ class Solution(object):
             index -= 1
         if m < 0:
             nums1[:n + 1] = nums2[:n + 1]
+            
+# 方法二，来自 辰星
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        total = m + n - 1
+        m, n = m - 1, n - 1
+        for i in range(total, -1, -1):
+            if n < 0:
+                break
+            elif m < 0:
+                nums1[ : n + 1] = nums2[: n + 1]
+                break            
+            elif nums1[m] > nums2[n]:
+                nums1[i] = nums1[m]
+                m -= 1
+            else:
+                nums1[i] = nums2[n]
+                n -= 1
